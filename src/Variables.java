59445d2fe224d4e72ae7959d1fb853173bc8241f
Rafael Hinojosa <rhinojosamoreno@gmail.com>
2022-07-14 11:55:09 -0500

    Primer commit del proyecto

---
+++ src/Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Declaroando variables
+        int speed;
+        // Asignamos valor a variable
+        speed = 10;
+        System.out.println(speed);
+
+        int salary = 1000;
+        String employeeName = "Jesus";
+        System.out.println(employeeName);
+
+    }
+}
