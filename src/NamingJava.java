eeffae74f043db59eb84ba3c8a2de2622620bb2b
Rafael Hinojosa <rhinojosamoreno@gmail.com>
2022-07-14 12:30:01 -0500

    Conversion de Nombrea a Java

--- src/NamingJava.java
+++ src/NamingJava.java
@@ -1,3 +1,19 @@
 public class NamingJava {
+    public static void main(String[] args) {
+        int celphone = 333123456;
+        int celPhone = 555123456;
+        System.out.println(celPhone);
+        System.out.println(celphone);
 
+        String $cuntryName = "Spanish";
+        String _backgroundColor = "Green";
+
+        String currency$ = "MXN";
+        String background_color = "Blue";
+
+        int POSITION = -5;
+        int MAX_width = 9999;
+        int MIN_WIDTH = 1;
+
+    }
 }
