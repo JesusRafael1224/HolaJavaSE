84f02b7b72f53f01582baba207a0938747389c2c
Rafael Hinojosa <rhinojosamoreno@gmail.com>
2022-07-14 22:55:40 -0500

    Se documentaron las funciones

--- src/Funciones.java
+++ src/Funciones.java
@@ -17,16 +17,40 @@ public class Funciones {
         System.out.println(esferaVolumen);
 
         System.out.println("Pesos a Dolares" + converToDollar(100, "COP"));
+
     }
+    /**
+     * Descripcion: Función que calcula el area de un circulo
+     * @param r parametro que se toma para calcular el area
+     * @return Se retorna el resultado
+     * */
     public static double circleArea(double r){
         return Math.PI * Math.pow(r,2);
     }
+    /**
+     * Descripcion: Función que calcula el area de una esfera
+     * @param r parametro que se toma para calcular el area de una esfera
+     * @return Se retorna el resultado
+     * */
     public static double sphereArea(double r){
         return 4 * Math.PI * Math.pow(r,2);
     }
+    /**
+     * Descripcion: Función que calcula el volumen de una esfera
+     * @param r parametro que se toma para calcular el volumen de una esfera
+     * @return Se retorna el resultado
+     * */
     public static double sphereVolumen(double r){
         return (4/3) * Math.PI * Math.pow(r,3);
     }
+
+    /**
+     * Descripción: Función que especificando su moneda convierte una cantidas de dienro a dolares
+     *
+     * @param quantity Cantidad de dinero
+     * @param currency Tipo de Moneda: Solo acepta MXN o COP
+     * @return quantity Devuelve la cantidad actualizada en Dolares
+     * */
     public static double converToDollar(double quantity, String currency){
         //MXN COP
         switch (currency){
