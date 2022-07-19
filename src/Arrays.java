03a8e74fb07da1f5aaa37b0cd2fe264862079f21
Rafael Hinojosa <rhinojosamoreno@gmail.com>
2022-07-18 20:54:48 -0500

    Arrays

---
+++ src/Arrays.java
@@ -0,0 +1,23 @@
+public class Arrays {
+    public static void main(String[] args) {
+        String[] androidVersions = new String[17];
+        String days[] = new String[7];
+
+        String[][] cities = new String[4][2];
+
+        int [][][] numbers = new int[2][2][2];
+        int [][][][] numbers4 = new int[2][2][2][2];
+
+        androidVersions[0]= "Apple Pie";
+        androidVersions[1]= "Bannana Bread";
+        androidVersions[2]= "Cupcake";
+        androidVersions[3]= "Donut";
+
+        System.out.println(androidVersions[0]);
+        System.out.println(androidVersions[1]);
+        System.out.println(androidVersions[3]);
+        System.out.println(androidVersions[2]);
+
+
+    }
+}
